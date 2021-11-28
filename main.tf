9e4ec8bd1f523d4f8c18d73152e79374715d8927
Ravi Senevirathne <85973309+ravisenevirathne@users.noreply.github.com>
2021-11-29 01:22:28 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 variable "vpc_cidr_block" {default = "10.0.0.0/16"}
 variable "subnet_cidr_block" {default = "10.0.10.0/24"}
-variable "avail_zone" {default = "ap-southeast-2"}
+variable "avail_zone" {default = "ap-southeast-2a"}
 variable "env_prefix" {default = "dev"}
 variable "my_ip" {default = "211.26.246.72/32"}
 variable "instance_type" {default = "t2.micro"}
