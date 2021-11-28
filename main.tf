618d634ff71d0a4e647c44b9ca44f54cd2d697f4
Ravi Senevirathne <85973309+ravisenevirathne@users.noreply.github.com>
2021-11-29 01:11:12 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ env_prefix = "dev"
 my_ip = "211.26.246.72/32"
 instance_type = "t2.micro"
 //public_key_location = "/c/Users/ravi.senevirathne/.ssh/server-key.pub"
-public_key_location = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9YngnhC5subr0eElN1kRW9pBcJnrJCWOHogfzDUarYQwhgbF/tTuGIdqVKHBekRrand9U6IRxXT9c6DsjspbEGgJuJDd53BIecWfO/y836gCyIZRwAPoK5/r/sBdolgYdJyex+3VGEgmfLhN7ulmDm9oSpcKyVefZGzk8d3/xR3PeC/Vxb0QCyWLrltTNapv9HOYuOsH7XGG2XnOFEhS2CXhIWMYYww9QCn5JweDEOaSCtrPSD6RUVFruDCaEB94//rY3a0gfXSU+IEATQ9WXfrNL6R1FCzAyDpnEZRjqsRtab71+TfJq2p1WNzG+NkSM1kmAQo+d488nEDj0riwFKF9+0VT9s0Xh2VA1q88Czp6BkXyKKO8Bp8TQ90wfCmHf47xHfQkt+mp5twkgPJnwImgl1ZqKH1Ad5B4PiiI6FO1OGadUk5a8LDBUMmmHi1t1veNw68pSuTzylvkQqH7nqyarIaKx4vTaKxjaJHolFZAJcg9nutcTVhWpz7ow0K0= Ravi.Senevirathne@VL000090"
+//public_key_location = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9YngnhC5subr0eElN1kRW9pBcJnrJCWOHogfzDUarYQwhgbF/tTuGIdqVKHBekRrand9U6IRxXT9c6DsjspbEGgJuJDd53BIecWfO/y836gCyIZRwAPoK5/r/sBdolgYdJyex+3VGEgmfLhN7ulmDm9oSpcKyVefZGzk8d3/xR3PeC/Vxb0QCyWLrltTNapv9HOYuOsH7XGG2XnOFEhS2CXhIWMYYww9QCn5JweDEOaSCtrPSD6RUVFruDCaEB94//rY3a0gfXSU+IEATQ9WXfrNL6R1FCzAyDpnEZRjqsRtab71+TfJq2p1WNzG+NkSM1kmAQo+d488nEDj0riwFKF9+0VT9s0Xh2VA1q88Czp6BkXyKKO8Bp8TQ90wfCmHf47xHfQkt+mp5twkgPJnwImgl1ZqKH1Ad5B4PiiI6FO1OGadUk5a8LDBUMmmHi1t1veNw68pSuTzylvkQqH7nqyarIaKx4vTaKxjaJHolFZAJcg9nutcTVhWpz7ow0K0= Ravi.Senevirathne@VL000090"
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -107,10 +107,12 @@ output "ec2_public_ip" {
   value = aws_instance.myapp-server.public_ip
 }
 
+/*
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"
   public_key = var.public_key_location
 }
+*/
 
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
@@ -121,7 +123,7 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
+  //key_name = aws_key_pair.ssh-key.key_name
 
   user_data = <<EOF
                     #!/bin/bash
